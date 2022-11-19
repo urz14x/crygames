05fa23d123018b8dd24b861a1e7d168da5afcb61
Ustami Razib <ustamirazib45@gmail.com>
2022-11-19 15:40:16 +0700

    Menghapus h1 di folder layout/container

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -4,7 +4,6 @@ const Container = (props: PropsWithChildren) => {
   return (
     <div className="pt-10 flex justify-center items-center min-h-screen">
       {props.children}
-      <h1>Word</h1>
     </div>
   );
 };
