5f77079de9d718cf2a9cdee8adb814e782d984de
Ustami Razib <ustamirazib45@gmail.com>
2022-11-19 15:37:45 +0700

    Menambahkan h1 ke Container di folder layout

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -4,6 +4,7 @@ const Container = (props: PropsWithChildren) => {
   return (
     <div className="pt-10 flex justify-center items-center min-h-screen">
       {props.children}
+      <h1>Word</h1>
     </div>
   );
 };
